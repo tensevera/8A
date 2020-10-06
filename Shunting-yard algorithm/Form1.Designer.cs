3362066e7368966183966dce6ba4a7c9c42d421e
tensevera <58250413+tensevera@users.noreply.github.com>
2020-10-06 19:23:23 +0200

    Innitial Commit - Shunting yard algorithm

---
+++ Shunting-yard algorithm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Shunting_yard_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbVstup = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbVstup
+            // 
+            this.tbVstup.Location = new System.Drawing.Point(63, 12);
+            this.tbVstup.Name = "tbVstup";
+            this.tbVstup.Size = new System.Drawing.Size(220, 23);
+            this.tbVstup.TabIndex = 0;
+            this.tbVstup.Text = "VSTUP";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(289, 11);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.tbVstup);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbVstup;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
---
+++ Shunting-yard algorithm/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shunting_yard_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stack<double> cisla = new Stack<double>(); //vytvořím zásobník na čísla
+        Stack<char> operace = new Stack<char>(); //vytvořím zásobník na operace
+
+        private void btStart_Click(object sender, EventArgs e) //zapnu program
+        {
+            for (int i = 0; i < tbVstup.Text.Length; i++) //projdu program
+            {
+                if (tbVstup.Text[i] > 47 && tbVstup.Text[i] < 58) //cislo
+                {
+                    cisla.Push(tbVstup.Text[i] - 48); //pridam cislo do zasobniku
+                }
+                else
+                {
+                    if (tbVstup.Text[i] == '(') //pokud dojde na zavorku
+                    {
+                        operace.Push('(');
+                    }
+                    else
+                    {
+                        if (tbVstup.Text[i] == '*' ||tbVstup.Text[i] =='/') //dojde na znamenka vyssi priority
+                            {
+                                while (operace.Count > 0 && (operace.Peek() == '*' || operace.Peek() == '/')) //odkud muzu vystrilim zasobnik
+                                {
+                                pocitej(operace.Pop());
+                                }
+                                operace.Push(tbVstup.Text[i]); //pridam nakonec
+                            }
+                        else
+                        {
+                            if (tbVstup.Text[i] == '+' || tbVstup.Text[i] == '-') //dojde na znemenka nizsi priority
+                            {
+                                while (operace.Count > 0 && operace.Peek() != '(') //odkud muzu vystrilim zasobnik
+                                {
+                                    pocitej(operace.Pop());
+                                }
+                                operace.Push(tbVstup.Text[i]); //pridam nakonec
+                            }
+                            else
+                            {
+                                if(tbVstup.Text[i] == ')') //dostanu pravou zavorku
+                                {
+                                    while(operace.Count > 0 && operace.Peek() != '(') //projdu zasobnik az do leve zavorky
+                                    {
+                                        pocitej(operace.Pop()); //vystrilim
+                                    }
+                                    if (operace.Count > 0)
+                                    {
+                                        operace.Pop(); //zbavim se leve zavorky
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            while (operace.Count > 0) //vystrilim zbytek zasobniku
+            {
+                pocitej(operace.Pop());
+            }
+            tbVstup.Text =(cisla.Pop()).ToString(); //vypisu
+        }
+        public void pocitej(char znamenko) //funkce pocitej
+        {
+            if (znamenko == '+') //pro scitani
+            {
+                cisla.Push(cisla.Pop() + cisla.Pop());
+            }
+            else
+            {
+                if (znamenko == '-') //pro odcitani
+                {
+                    double a = cisla.Pop();
+                    cisla.Push(cisla.Pop() - a);
+                }
+                else
+                {
+                    if (znamenko == '*') //pro nasobeni
+                    {
+                        cisla.Push(cisla.Pop() * cisla.Pop());
+                    }
+                    else //pro deleni
+                    {
+                        double a = cisla.Pop();
+                        cisla.Push(cisla.Pop() / a);
+                    }
+                }
+            }
+        }
+    }
+}
